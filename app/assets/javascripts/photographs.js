d31e39f1fb6abad821eeea9b5424c31104768e3c
flanban <johnsfuller@gmail.com>
2014-05-13 11:46:53 -0400

    first commit

---
+++ app/assets/javascripts/photographs.js
@@ -0,0 +1,23 @@
+var url = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=efeecfff9bc2f0f26ca576d1293ce4aa&user_id=24881537%40N02&format=json&nojsoncallback=1&auth_token=72157644240953428-3599714826103e5e&api_sig=d45db628b9143277c4b272082cb95f45'
+
+var flickerAPI = url;
+  $.getJSON( flickerAPI, {
+    format: "json"
+  })
+    .done(function( data ) {
+      console.log(JSON.stringify(url));
+      var list = $("<ul></ul>");
+      
+      $.each( data.photosets.photoset, function( i, item ) {
+        var link = $("<a/>").attr("title", item.description._content)
+                    .attr("href", "http://www.flickr.com/photos/mjryall/sets/" + item.id)
+                    .text(item.title._content);
+        var li = $("<li/>").append(link).append(" (" + item.photos + ")");
+                $(list).append(li);
+        if (i >= 11) {
+          return false
+        }
+      });
+      $("#images").append(list);
+      
+    });
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NatesPhotos::Application.config.secret_key_base = 'a10560f8582c9635b2c14cc7543aa904958cad27ff2876fd4cec47b977f5c26e4e959f4e30d3b2bbc9cf59b54b24683373142e25a7cefbbc9394587d7753d8a3'
