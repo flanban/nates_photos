3f9dfd87250238ebd567881578a83f8aed988b7a
flanban <johnsfuller@gmail.com>
2014-05-21 15:05:51 -0400

    set detail page height

---
+++ app/assets/javascripts/detail-page.js
@@ -0,0 +1,14 @@
+function setDetailPageHeight() {
+  menuHeight = $('#detail-page-menu').outerHeight()
+  detailPageHeightWithMenu = $('#detail-page').height() - menuHeight
+  $('#detail-page').height(detailPageHeightWithMenu)
+}
+$(function() {
+  setDetailPageHeight();
+
+  //resize event
+  $(window).resize(function(){
+    setDetailPageHeight();
+  });
+
+});
