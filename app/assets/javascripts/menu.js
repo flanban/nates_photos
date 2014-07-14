1f72fa76ea00e0fc9609eaa46bae0277d3640a35
flanban <johnsfuller@gmail.com>
2014-07-14 10:32:51 -0700

    style footer

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,16 +1,30 @@
 // set active nav state
 $(document).ready(function() {
- 
-// store url for current page as global variable
-current_page = document.location.href
- 
-// apply active states depending on current page
-if (current_page.match(/photography/)) {
-  $("#primary-menu li:eq(0) a").addClass('active');
-} else if (current_page.match(/cinematography/)) {
-  $("#primary-menu li:eq(1) a").addClass('active');
-} else { // don't mark any nav links as active
-  $("#primary-menu li").removeClass('active');
-};
+  
+   // store url for current page as global variable
+  current_page = document.location.href
+  
+  // apply active states depending on current page
+  if (current_page.match(/photography/)) {
+    $("#primary-menu li:eq(0) a").addClass('active');
+  } else if (current_page.match(/cinematography/)) {
+    $("#primary-menu li:eq(1) a").addClass('active');
+  } else { // don't mark any nav links as active
+    $("#primary-menu li").removeClass('active');
+  };
+});
 
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 700);
+        return false;
+      }
+    }
+  });
 });
